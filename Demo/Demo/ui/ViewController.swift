c670fb0d37d2957f826b73200c38d08478702113
ThaoHTN <thaohtn@mti-tech.vn>
2019-04-18 09:22:59 +0700

    Rebase Git test commit branch test3

--- Demo/Demo/ui/ViewController.swift
+++ Demo/Demo/ui/ViewController.swift
@@ -14,6 +14,7 @@ class ItemViewCell: UICollectionViewCell {
     @IBOutlet weak var itemName: UILabel!
 }
 
+// Rebase Git test commit branch test3
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     @IBOutlet weak var collectionView: UICollectionView!
     
